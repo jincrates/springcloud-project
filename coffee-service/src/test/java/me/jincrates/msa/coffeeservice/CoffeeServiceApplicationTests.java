7bb85124e7334b5fab80b4606383b83968b1c9ec
jincrates <94jingyu@gmail.com>
2023-07-12 22:51:36 +0900

    feat: project init

---
+++ coffee-service/src/test/java/me/jincrates/msa/coffeeservice/CoffeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jincrates.msa.coffeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
