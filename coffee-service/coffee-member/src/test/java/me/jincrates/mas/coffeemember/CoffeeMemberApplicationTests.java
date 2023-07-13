256e6adb41fc1a23270f434e3ad52d20cb58d197
jincrates <94jingyu@gmail.com>
2023-07-13 22:42:26 +0900

    feat: 유저 모듈 추가

---
+++ coffee-service/coffee-member/src/test/java/me/jincrates/mas/coffeemember/CoffeeMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jincrates.mas.coffeemember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
