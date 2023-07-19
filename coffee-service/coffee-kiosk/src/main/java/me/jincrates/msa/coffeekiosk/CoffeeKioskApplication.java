3ce41d5afa8f05f6e299253103d575ea3ddee49b
jincrates <94jingyu@gmail.com>
2023-07-19 22:54:20 +0900

    feat: 키오스크 모듈 추가

---
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/CoffeeKioskApplication.java
@@ -0,0 +1,13 @@
+package me.jincrates.msa.coffeekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeKioskApplication.class, args);
+	}
+
+}
---
+++ coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/CoffeeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jincrates.msa.coffeekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
