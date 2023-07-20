c41b83bac5d20a48139857a26a4e758d270ffe10
jincrates <94jingyu@gmail.com>
2023-07-20 19:00:39 +0900

    test: order entity 설계중

---
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/domain/order/Order.java
@@ -0,0 +1,32 @@
+package me.jincrates.msa.coffeekiosk.spring.domain.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jincrates.msa.coffeekiosk.spring.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+@Entity
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredAt;
+}
---
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/domain/orderprodct/OrderProduct.java
@@ -0,0 +1,20 @@
+package me.jincrates.msa.coffeekiosk.spring.domain.orderprodct;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jincrates.msa.coffeekiosk.spring.domain.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
