a0ac1c1b1e57f51441aa0bf35352b2050494519c
jincrates <94jingyu@gmail.com>
2023-07-20 15:10:28 +0900

    feat: DisplayName을 섬세하게 작성하기

--- coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskTest.java
+++ coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskTest.java
@@ -7,8 +7,36 @@ import java.time.LocalDateTime;
 import me.jincrates.msa.coffeekiosk.unit.beverage.Americano;
 import me.jincrates.msa.coffeekiosk.unit.beverage.Latte;
 import me.jincrates.msa.coffeekiosk.unit.order.Order;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * [ DisplayName을 섬세하게 ]
+ * <p>
+ * 1. 명사의 나열보다는 문장으로 작성하기("~테스트"는 지양하기)
+ * <p>
+ * - A이면 B이다.
+ * <p>
+ * - A이면 B가 아니고 C다.
+ * <p>
+ * 2. 테스트 행위에 대한 결과까지 기술하기
+ * <p>
+ * - 음료를 1개 추가할 수 있다. (X)
+ * <p>
+ * - 음료를 1개 추가하면 주문 목록에 담긴다. (O)
+ * <p>
+ * 3. 도메인 용어를 사용하여 한층 추상화된 내용을 담기
+ * <p>
+ * - 특정 시간 이전에 주문을 생성하면 실패한다. (X)
+ * <p>
+ * - 영업 시작 시간 이전에는 주문을 생성할 수 없다. (O)
+ * <p>
+ * 4. 테스트의 현상을 중점으로 기술하지 말 것
+ * <p>
+ * - ~하면 성공/실패한다. (X)
+ * <p>
+ * - ~하면 ~~하다. (O)
+ */
 class CoffeeKioskTest {
 
     @Test
@@ -21,6 +49,8 @@ class CoffeeKioskTest {
     }
 
     @Test
+    //@DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개 추가하면 주문 목록에 담긴다.")
     void add() {
         CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
         coffeeKiosk.add(new Americano());
