345d5040e563f9adfa8fd15d88c21ea1158cae1f
jincrates <94jingyu@gmail.com>
2023-07-20 14:28:07 +0900

    test: 테스트하기 어려운 코드 -> 외부로 분리

--- coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskRunner.java
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskRunner.java
@@ -1,21 +1,25 @@
 package me.jincrates.msa.coffeekiosk.unit;
 
+import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import me.jincrates.msa.coffeekiosk.unit.beverage.Americano;
 import me.jincrates.msa.coffeekiosk.unit.beverage.Latte;
+import me.jincrates.msa.coffeekiosk.unit.order.Order;
 
 @Slf4j
 public class CoffeeKioskRunner {
 
     public static void main(String[] args) {
         CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
-        coffeeKiosk.add(new Americano(), 1);
+        coffeeKiosk.add(new Americano());
         log.info(">>> 아메리카노 추가");
 
-        coffeeKiosk.add(new Latte(), 1);
+        coffeeKiosk.add(new Latte());
         log.info(">>> 라떼 추가");
 
         int totalPrice = coffeeKiosk.calculateTotalPrice();
         log.info("총 주문가격: {}", totalPrice);
+
+        Order order = coffeeKiosk.createOrder(LocalDateTime.now());
     }
 }
--- coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskTest.java
+++ coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskTest.java
@@ -3,8 +3,10 @@ package me.jincrates.msa.coffeekiosk.unit;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDateTime;
 import me.jincrates.msa.coffeekiosk.unit.beverage.Americano;
 import me.jincrates.msa.coffeekiosk.unit.beverage.Latte;
+import me.jincrates.msa.coffeekiosk.unit.order.Order;
 import org.junit.jupiter.api.Test;
 
 class CoffeeKioskTest {
@@ -12,7 +14,7 @@ class CoffeeKioskTest {
     @Test
     void add_manual_test() {
         CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
-        coffeeKiosk.add(new Americano(), 1);
+        coffeeKiosk.add(new Americano());
 
         System.out.println(">>> 담긴 음료 수: " + coffeeKiosk.getBeverages().size());
         System.out.println(">>> 담긴 음료: " + coffeeKiosk.getBeverages().get(0).getName());
@@ -21,7 +23,7 @@ class CoffeeKioskTest {
     @Test
     void add() {
         CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
-        coffeeKiosk.add(new Americano(), 1);
+        coffeeKiosk.add(new Americano());
 
         assertThat(coffeeKiosk.getBeverages()).hasSize(1);
         assertThat(coffeeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
@@ -49,13 +51,12 @@ class CoffeeKioskTest {
             .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
     }
 
-
     @Test
     void remove() {
         CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
         Americano americano = new Americano();
 
-        coffeeKiosk.add(americano, 1);
+        coffeeKiosk.add(americano);
         assertThat(coffeeKiosk.getBeverages()).hasSize(1);
 
         coffeeKiosk.remove(americano);
@@ -68,11 +69,60 @@ class CoffeeKioskTest {
         Americano americano = new Americano();
         Latte latte = new Latte();
 
-        coffeeKiosk.add(americano, 1);
-        coffeeKiosk.add(latte, 1);
+        coffeeKiosk.add(americano);
+        coffeeKiosk.add(latte);
         assertThat(coffeeKiosk.getBeverages()).hasSize(2);
 
         coffeeKiosk.clear();
         assertThat(coffeeKiosk.getBeverages()).isEmpty();
     }
+
+    @Test
+    void createOrder() {
+        CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
+        Americano americano = new Americano();
+        coffeeKiosk.add(americano);
+
+        Order order = coffeeKiosk.createOrder();
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void createOrderWithCurrentTime() {
+        CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
+        Americano americano = new Americano();
+        coffeeKiosk.add(americano);
+
+        Order order = coffeeKiosk.createOrder(LocalDateTime.of(2023, 7, 20, 10, 0));
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void createOrderOutsideOpenTime() {
+        CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
+        Americano americano = new Americano();
+        coffeeKiosk.add(americano);
+
+        assertThatThrownBy(() -> coffeeKiosk.createOrder(LocalDateTime.of(2023, 7, 20, 9, 59)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
