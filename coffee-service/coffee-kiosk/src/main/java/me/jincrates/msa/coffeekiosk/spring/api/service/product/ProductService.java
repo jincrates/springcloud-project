6e62846220376b7c4a28328897305295fa82ebc4
jincrates <94jingyu@gmail.com>
2023-07-25 23:20:01 +0900

    test: 트랜잭션 readOnly를 알맞게 잘 사용하자

--- coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/api/service/product/ProductService.java
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/api/service/product/ProductService.java
@@ -9,15 +9,28 @@ import me.jincrates.msa.coffeekiosk.spring.domain.product.Product;
 import me.jincrates.msa.coffeekiosk.spring.domain.product.ProductRepository;
 import me.jincrates.msa.coffeekiosk.spring.domain.product.ProductSellingStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+/**
+ * readOnly = true : 읽기 전용
+ * <p>
+ * CRUD 에서 CUD 동작 X / only Read
+ * <p>
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ * <p>
+ * <p>
+ * CQRS - Command / Query를 분리하는 패턴
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
     // 동시성 이슈
     // 증가값이 아닌 UUID를 할당하는 것도 하나의 방법
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
         String nextProductNumber = createNextProductNumber();
 
