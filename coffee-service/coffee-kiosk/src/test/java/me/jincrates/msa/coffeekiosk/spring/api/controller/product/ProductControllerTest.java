22ec70d0a7dd44924464bbb2e031bcb5a0ce9fa3
jincrates <94jingyu@gmail.com>
2023-07-26 23:51:29 +0900

    test: static으로 정리

--- coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/spring/api/controller/product/ProductControllerTest.java
+++ coffee-service/coffee-kiosk/src/test/java/me/jincrates/msa/coffeekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -1,19 +1,21 @@
 package me.jincrates.msa.coffeekiosk.spring.api.controller.product;
 
+import static me.jincrates.msa.coffeekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static me.jincrates.msa.coffeekiosk.spring.domain.product.ProductType.HANDMADE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.jincrates.msa.coffeekiosk.spring.api.controller.product.request.ProductCreateRequest;
 import me.jincrates.msa.coffeekiosk.spring.api.service.product.ProductService;
-import me.jincrates.msa.coffeekiosk.spring.domain.product.ProductSellingStatus;
-import me.jincrates.msa.coffeekiosk.spring.domain.product.ProductType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(controllers = ProductController.class)
 class ProductControllerTest {
@@ -32,17 +34,19 @@ class ProductControllerTest {
     void createProduct() throws Exception {
         // given
         ProductCreateRequest request = ProductCreateRequest.builder()
-            .type(ProductType.HANDMADE)
-            .sellingStatus(ProductSellingStatus.SELLING)
+            .type(HANDMADE)
+            .sellingStatus(SELLING)
             .name("아메리카노")
             .price(4000)
             .build();
 
         // when // then
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/products/new")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request))
+        mockMvc.perform(
+                post("/api/v1/products/new")
+                    .contentType(APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
             )
-            .andExpect(MockMvcResultMatchers.status().isOk());
+            .andDo(print())
+            .andExpect(status().isOk());
     }
 }
