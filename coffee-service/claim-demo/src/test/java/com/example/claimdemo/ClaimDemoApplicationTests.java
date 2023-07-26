a866bcbd560e5f5bda67f37924588dbaf4263d2a
jincrates <94jingyu@gmail.com>
2023-07-26 16:45:47 +0900

    feat: 테스트 모듈 init

---
+++ coffee-service/claim-demo/src/test/java/com/example/claimdemo/ClaimDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claimdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
