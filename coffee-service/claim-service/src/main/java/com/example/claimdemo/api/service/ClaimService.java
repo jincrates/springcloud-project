27ded2697104e0d2b8942112f260117d3782f8b7
jincrates <94jingyu@gmail.com>
2023-08-12 11:57:29 +0900

    fix: change module naming

--- coffee-service/claim-service/src/main/java/com/example/claimdemo/api/service/ClaimService.java
+++ coffee-service/claim-service/src/main/java/com/example/claimdemo/api/service/ClaimService.java
@@ -2,7 +2,6 @@ package com.example.claimdemo.api.service;
 
 import com.example.claimdemo.api.service.request.ClaimCreateServiceRequest;
 import com.example.claimdemo.api.service.request.OrderProductServiceRequest;
-import com.example.claimdemo.domain.orderproduct.OrderProduct;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,8 +17,8 @@ public class ClaimService {
             .map(OrderProductServiceRequest::getOrderProductId)
             .toList();
 
-        List<OrderProduct> orderProducts = orderProductRepository.findByOrderIdAndIdIn(
-            request.getOrderId(), orderProductIds);
+//        List<OrderProduct> orderProducts = orderProductRepository.findByOrderIdAndIdIn(
+//            request.getOrderId(), orderProductIds);
 
         // 클레임 저장
     }
