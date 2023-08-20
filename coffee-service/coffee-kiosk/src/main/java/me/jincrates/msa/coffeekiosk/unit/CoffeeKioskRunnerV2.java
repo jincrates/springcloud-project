4c987dd3d9cefcad41958b2c9af4c58fdbc0e39e
jincrates <94jingyu@gmail.com>
2023-08-20 21:38:59 +0900

    fix: 메뉴 출력 수정

--- coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskRunnerV2.java
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/unit/CoffeeKioskRunnerV2.java
@@ -2,6 +2,7 @@ package me.jincrates.msa.coffeekiosk.unit;
 
 import me.jincrates.msa.coffeekiosk.unit.beverage.v2.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,29 +13,72 @@ public class CoffeeKioskRunnerV2 {
         // 웰컴 메시지
         System.out.println("커피 주문 프로그램에 오신 것을 환영합니다!");
 
-        // 커피 메뉴 생성
-        System.out.println("커피 종류를 선택하세요:");
         Menu menu = createMenu();
-        for (int i = 1; i <= menu.getList().size(); i++) {
-            System.out.println(i + ". " + menu.getList().get(i - 1).getName());
-        }
-        // 메뉴 선택
-        int choice = scanner.nextInt();
-        Coffee selectedCoffee = menu.getList().get(choice - 1);
+        List<Coffee> cart = new ArrayList<>();
+        while (true) {
+            // 커피 메뉴 생성
+            System.out.println("커피 종류를 선택하세요:");
+            for (int i = 1; i <= menu.getList().size(); i++) {
+                System.out.println(i + ". " + menu.getList().get(i - 1).getName());
+            }
 
-        // 메뉴 옵션 선택
-        selectedMenuOption(scanner, selectedCoffee);
+            // 메뉴 선택
+            int choice = scanner.nextInt();
+            Coffee selectedCoffee = menu.getList().get(choice - 1);
+
+            // 카트 담기
+            cart.add(selectedCoffee);
+
+            // 메뉴 옵션 선택
+            selectedMenuOption(scanner, selectedCoffee);
+
+            System.out.println("추가로 더 주문하시겠습니까? (Y/N)");
+            String addedChoice = scanner.next();
+            if (!addedChoice.equalsIgnoreCase("Y")) {
+                break;
+            }
+        }
 
         // 주문내역 출력
-        System.out.println("==============================================");
-        System.out.println("주문 내역:");
-        System.out.println(selectedCoffee);
-        System.out.println("가격: " + selectedCoffee.calculatePrice() + "원");
+        printHistory(cart);
 
         scanner.close();
     }
 
+    private static void printHistory(List<Coffee> cart) {
+        int totalPrice = 0;
+
+        String line = "==============================================";
+        System.out.println(line);
+        System.out.println("주문 내역:");
+        for (Coffee coffee : cart) {
+            totalPrice += coffee.calculatePrice();
+            System.out.println(coffee + " --- " + coffee.calculatePrice() + "원");
+        }
+
+        System.out.println("총 가격: " + totalPrice + "원");
+
+    }
+
     private static void selectedMenuOption(Scanner scanner, Coffee selectedCoffee) {
+        System.out.println("커피 온도를 선택하세요:");
+        List<Temperature> temperatureList = selectedCoffee.getEnableOption().getTemperatures();
+        for (int i = 1; i <= temperatureList.size(); i++) {
+            System.out.println(i + ". " + temperatureList.get(i - 1).getText());
+        }
+        int temperatureChoice = scanner.nextInt();
+        Temperature selectedTemperature = temperatureList.get(temperatureChoice - 1);
+        selectedCoffee.temperature(selectedTemperature);
+
+        System.out.println("커피 사이즈를 선택하세요:");
+        List<Size> sizeList = selectedCoffee.getEnableOption().getSizes();
+        for (int i = 1; i <= sizeList.size(); i++) {
+            System.out.println(i + ". " + sizeList.get(i - 1).getText());
+        }
+        int sizeChoice = scanner.nextInt();
+        Size selectedSize = sizeList.get(sizeChoice - 1);
+        selectedCoffee.size(selectedSize);
+
         if (selectedCoffee.getEnableOption().isWhippedCream()) {
             System.out.println("휘핑크림 추가하시겠습니까? (Y/N)");
             String whippedCreamChoice = scanner.next();
@@ -58,26 +102,6 @@ public class CoffeeKioskRunnerV2 {
                 selectedCoffee.makeDecaf();
             }
         }
-
-        // 사이즈 선택
-        System.out.println("커피 사이즈를 선택하세요:");
-        List<Size> sizeList = selectedCoffee.getEnableOption().getSizes();
-        for (int i = 1; i <= sizeList.size(); i++) {
-            System.out.println(i + ". " + sizeList.get(i - 1).getText());
-        }
-        int sizeChoice = scanner.nextInt();
-        Size selectedSize = sizeList.get(sizeChoice - 1);
-        selectedCoffee.size(selectedSize);
-
-
-        System.out.println("커피 온도를 선택하세요:");
-        List<Temperature> temperatureList = selectedCoffee.getEnableOption().getTemperatures();
-        for (int i = 1; i <= temperatureList.size(); i++) {
-            System.out.println(i + ". " + temperatureList.get(i - 1).getText());
-        }
-        int temperatureChoice = scanner.nextInt();
-        Temperature selectedTemperature = temperatureList.get(temperatureChoice - 1);
-        selectedCoffee.temperature(selectedTemperature);
     }
 
     private static Menu createMenu() {
