23becd9751ca08fb5aca4c8d7103e0e5ffacba62
jincrates <94jingyu@gmail.com>
2023-08-31 21:17:10 +0900

    feat: handler 처리

---
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/global/common/enumtype/EnumType.java
@@ -0,0 +1,7 @@
+package me.jincrates.msa.coffeekiosk.spring.global.common.enumtype;
+
+public interface EnumType {
+    String getKey();
+
+    String getValue();
+}
--- coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/global/common/response/CommonResponse.java
+++ coffee-service/coffee-kiosk/src/main/java/me/jincrates/msa/coffeekiosk/spring/global/common/response/CommonResponse.java
@@ -2,6 +2,7 @@ package me.jincrates.msa.coffeekiosk.spring.global.common.response;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 public class CommonResponse<T> {
@@ -28,5 +29,10 @@ public class CommonResponse<T> {
 
     public static <T> CommonResponse<T> ok(T data) {
         return of(HttpStatus.OK, data);
+    }
+
+    public static ResponseEntity<?> toResponseEntity(HttpStatus status, String message) {
+        return ResponseEntity.status(status)
+                .body(CommonResponse.of(status, message, null));
     }
 }
