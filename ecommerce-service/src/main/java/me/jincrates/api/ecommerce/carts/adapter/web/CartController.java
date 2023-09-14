64d390fe22622d8f61b5b7b82231f955dd4b8c58
jincrates <94jingyu@gmail.com>
2023-09-14 23:01:27 +0900

    fix: UseCase 제거

--- ecommerce-service/src/main/java/me/jincrates/api/ecommerce/carts/adapter/web/CartController.java
+++ ecommerce-service/src/main/java/me/jincrates/api/ecommerce/carts/adapter/web/CartController.java
@@ -2,7 +2,7 @@ package me.jincrates.api.ecommerce.carts.adapter.web;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import me.jincrates.api.ecommerce.carts.adapter.web.usecase.CartUseCase;
+import me.jincrates.api.ecommerce.carts.application.service.CartService;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "장바구니 서비스", description = "장바구니 등록/조회 API")
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CartController {
 
-    private final CartUseCase cartUseCase;
+    private final CartService cartService;
 }
--- ecommerce-service/src/main/java/me/jincrates/api/ecommerce/carts/application/service/CartService.java
+++ ecommerce-service/src/main/java/me/jincrates/api/ecommerce/carts/application/service/CartService.java
@@ -1,10 +1,7 @@
 package me.jincrates.api.ecommerce.carts.application.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import me.jincrates.api.ecommerce.carts.adapter.web.usecase.CartUseCase;
 import me.jincrates.api.ecommerce.carts.application.port.CartPort;
 import me.jincrates.api.ecommerce.carts.application.service.request.CartProductServiceRequest;
 import me.jincrates.api.ecommerce.carts.application.service.response.CartDetailServiceResponse;
@@ -17,21 +14,23 @@ import me.jincrates.api.ecommerce.products.domain.product.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class CartService implements CartUseCase {
+public class CartService {
 
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
 
     private final CartPort cartPort;
 
-    @Override
     public Long addCart(CartProductServiceRequest request, String email) {
         Product product = productRepository.findById(request.getProductId())
-            .orElseThrow(() -> new EntityNotFoundException(
-                "상품을 찾을 수 없습니다. productId=" + request.getProductId()));
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "상품을 찾을 수 없습니다. productId=" + request.getProductId()));
         Member member = getMemberByEmail(email);
 
         Cart cart = cartPort.findCartByMemberId(member.getId());
@@ -41,7 +40,7 @@ public class CartService implements CartUseCase {
         }
 
         CartProduct savedCartProduct = cartPort.findCartProductByCartIdAndProductId(cart.getId(),
-            product.getId());
+                product.getId());
         if (savedCartProduct == null) {
             CartProduct cartProduct = CartProduct.create(cart, product, request.getQuantity());
             cartPort.saveCartProduct(cartProduct);
@@ -52,7 +51,6 @@ public class CartService implements CartUseCase {
         return savedCartProduct.getId();
     }
 
-    @Override
     public List<CartDetailServiceResponse> getCartDetails(String email) {
         Member member = getMemberByEmail(email);
 
@@ -64,14 +62,12 @@ public class CartService implements CartUseCase {
         return cartPort.findCartDetailsById(cart.getId());
     }
 
-    @Override
     public void updateCartProductQuantity(Long cartProductId, int quantity) {
         // TODO: 유저 인증 추가
         CartProduct cartProduct = cartPort.findCartProductById(cartProductId);
         cartProduct.updateQuantity(quantity);
     }
 
-    @Override
     public void deleteCartProduct(Long cartProductId) {
         // TODO: 유저 인증 추가
         CartProduct cartProduct = cartPort.findCartProductById(cartProductId);
@@ -80,6 +76,6 @@ public class CartService implements CartUseCase {
 
     private Member getMemberByEmail(String email) {
         return memberRepository.findByEmail(email)
-            .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다. email=" + email));
+                .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다. email=" + email));
     }
 }
