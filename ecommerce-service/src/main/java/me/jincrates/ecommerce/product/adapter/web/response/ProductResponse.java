89083ffd64a782b0dce993b96443d744c3fafcc0
jincrates <94jingyu@gmail.com>
2023-09-15 18:15:13 +0900

    test: 상품 등록 response 스웨거 추가

--- ecommerce-service/src/main/java/me/jincrates/ecommerce/product/adapter/web/response/ProductResponse.java
+++ ecommerce-service/src/main/java/me/jincrates/ecommerce/product/adapter/web/response/ProductResponse.java
@@ -1,5 +1,6 @@
 package me.jincrates.ecommerce.product.adapter.web.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -11,15 +12,27 @@ import me.jincrates.ecommerce.product.domain.Product;
 import me.jincrates.ecommerce.product.domain.ProductImage;
 import me.jincrates.ecommerce.product.domain.ProductSellingStatus;
 
+@Schema(description = "상품 response")
 @Getter
 public class ProductResponse {
 
-    private Long id;  // 상품 ID
-    private String productName;  // 상품명
+    @Schema(description = "상품 ID", example = "1")
+    private Long id;
+
+    @Schema(description = "상품명", example = "소크라테스의 변경")
+    private String productName;
+
+    @Schema(description = "상품 가격", example = "10000")
     private int price;
+
+    @Schema(description = "상품 상세 설명", example = "《소크라테스의 변명》은 인류 역사상 가장 위대한 철학자인 소크라테스의 영혼의 책이다. 제자 플라톤이 스승의 위대한 사상과 진실된 인간성을 널리 알리고 영원히 기리기 위해 심혈을 기울여 쓴 〈소크라테스의 변명〉 외에 〈크리톤〉, 〈파이돈〉, 〈향연〉을 함께 엮었다.")
     private String productDetail;  // 상품 상세설명
+
+    @Schema(description = "상품 판매상태", example = "SELLING")
     private ProductSellingStatus status;  // 상품 판매상태
+
     private List<ProductImageServiceResponse> productImages = new ArrayList<>();
+
     private List<Long> productImagesIds = new ArrayList<>();
 
     @Builder(access = AccessLevel.PRIVATE)
