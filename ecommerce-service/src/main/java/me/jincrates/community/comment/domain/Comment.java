31ba4dfdcc7081061ca15f72153d56cbf910cbe3
jincrates <94jingyu@gmail.com>
2023-10-18 17:11:40 +0900

    fix: 오류 수정

--- ecommerce-service/src/main/java/me/jincrates/community/comment/domain/Comment.java
+++ ecommerce-service/src/main/java/me/jincrates/community/comment/domain/Comment.java
@@ -1,18 +1,6 @@
 package me.jincrates.community.comment.domain;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +8,9 @@ import lombok.NoArgsConstructor;
 import me.jincrates.community.post.domain.Post;
 import me.jincrates.ecommerce.member.domain.Member;
 import me.jincrates.global.common.BaseTimeEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -35,14 +26,14 @@ public class Comment extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
-    @org.hibernate.annotations.Comment("게시글 ID")
-    private Post post;
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     @org.hibernate.annotations.Comment("작성자 ID")
     private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    @org.hibernate.annotations.Comment("게시글 ID")
+    private Post post;
 
     @Column(nullable = false, length = 200)
     @org.hibernate.annotations.Comment("댓글 내용")
@@ -57,19 +48,19 @@ public class Comment extends BaseTimeEntity {
     private List<Comment> replies = new ArrayList<>();
 
     @Builder(access = AccessLevel.PRIVATE)
-    public Comment(Post post, Member member, Comment parent, String content) {
-        this.post = post;
+    private Comment(Member member, Post post, Comment parent, String content) {
         this.member = member;
+        this.post = post;
         this.parent = parent;
         this.content = content;
     }
 
-    public static Comment create(Post post, Member member, String content) {
+    public static Comment create(Member member, Post post, String content) {
         return Comment.builder()
-            .post(post)
-            .member(member)
-            .content(content)
-            .build();
+                .member(member)
+                .post(post)
+                .content(content)
+                .build();
     }
 
     public void updateContent(String content) {
