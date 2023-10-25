6369445535cfb317153ff05bda2ae783c8eb082a
jincrates <94jingyu@gmail.com>
2023-10-25 22:43:31 +0900

    fix: 날짜 comment 추가

--- ecommerce-service/src/main/java/me/jincrates/global/common/BaseTimeEntity.java
+++ ecommerce-service/src/main/java/me/jincrates/global/common/BaseTimeEntity.java
@@ -23,11 +23,13 @@ public abstract class BaseTimeEntity {
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Comment("생성일시")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Comment("수정일시")
     private LocalDateTime updatedAt;
 
     public void setInactive() {
